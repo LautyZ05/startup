b5ae0f7b85a3dbcab0c93bf8e66a98b52f925d43
LautyZ05 <lautaro.s.zarate@gmail.com>
2025-10-08 21:14:05 -0600

    Added message/warning to start.jsx. also added to app.css

--- src/start/start.jsx
+++ src/start/start.jsx
@@ -6,6 +6,11 @@ export function Start() {
         <div className="div_index">
             <h1 className="h1_index">Welcome Player!</h1>
             <a href="select.html"><button type="submit" className="btn btn-lg btn-primary">Click to Enter</button></a>
+            <div className="startdiv">
+            <div>Warning: None of the buttons in the main area work.</div>
+            <div>They will send you to error page 404.</div>
+            <div>Use navigation buttons in header to go through the pages :)</div>
+            </div>
         </div>
     </main>
   );
