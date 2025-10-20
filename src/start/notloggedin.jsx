000d4cbafdbcbd44f5b5af14c64eaacfd3c18101
LautyZ05 <lautaro.s.zarate@gmail.com>
2025-10-20 16:13:52 -0600

    Created new file to test login features

--- src/start/notloggedin.jsx
+++ src/start/notloggedin.jsx
@@ -15,14 +15,9 @@ export function Start({setUser}) {
   return (
     <main>
         <div className="div_index">
-            <h1 className="h1_index">Welcome Player!</h1>
             <input type='text' onChange={textChange} />
             <button className="btn btn-primary btn-dark" onClick={LoginUser}>Login</button>
-            <div className="startdiv">
-              <div>Warning: None of the buttons in the main area work.</div>
-              <div>They will send you to error page 404.</div>
-              <div>Use navigation buttons in header to go through the pages :)</div>
-            </div>
+            
         </div>
     </main>
   );
