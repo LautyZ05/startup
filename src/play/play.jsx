9a76d1ad41b8715a277040a3fbe7a8800357c41f
LautyZ05 <lautaro.s.zarate@gmail.com>
2025-10-24 21:15:24 -0600

    Changed the scoreboard to be more interactive and added more code to play.jsx and created play.css to make board that still doesn't work

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,20 +1,63 @@
 import React from 'react';
+import "./play.css";
 
 export function Play({bgColor, textColor}) {
   return (
     <main style={{ background: bgColor, color: textColor}}>
         <section className="section_play">
             <h3>Score</h3>
-            <p>Numbers will go here</p>
+            <div>Numbers will go here</div>
+            <div>Still under construction</div>
         </section>
-        <section className="section_play">
-            <p>Game goes here</p>
-            <img alt="Temporary image of connect 4" src="temp_c4_image.svg" />
-            <p>Temporary image of what I want the game to look like</p>
-        </section>
-        <section className="section_play">
-            <p>controls go here</p>
-        </section>
+        <div id='board'>
+          <div className='circle' id="0/0"></div>
+          <div className='circle' id="0/1"></div>
+          <div className='circle' id="0/2"></div>
+          <div className='circle' id="0/3"></div>
+          <div className='circle' id="0/4"></div>
+          <div className='circle' id="0/5"></div>
+          <div className='circle' id="0/6"></div>
+
+          <div className='circle' id="1/0"></div>
+          <div className='circle' id="1/1"></div>
+          <div className='circle' id="1/2"></div>
+          <div className='circle' id="1/3"></div>
+          <div className='circle' id="1/4"></div>
+          <div className='circle' id="1/5"></div>
+          <div className='circle' id="1/6"></div>
+
+          <div className='circle' id="2/0"></div>
+          <div className='circle' id="2/1"></div>
+          <div className='circle' id="2/2"></div>
+          <div className='circle' id="2/3"></div>
+          <div className='circle' id="2/4"></div>
+          <div className='circle' id="2/5"></div>
+          <div className='circle' id="2/6"></div>
+
+          <div className='circle' id="3/0"></div>
+          <div className='circle' id="3/1"></div>
+          <div className='circle' id="3/2"></div>
+          <div className='circle' id="3/3"></div>
+          <div className='circle' id="3/4"></div>
+          <div className='circle' id="3/5"></div>
+          <div className='circle' id="3/6"></div>
+
+          <div className='circle' id="4/0"></div>
+          <div className='circle' id="4/1"></div>
+          <div className='circle' id="4/2"></div>
+          <div className='circle' id="4/3"></div>
+          <div className='circle' id="4/4"></div>
+          <div className='circle' id="4/5"></div>
+          <div className='circle' id="4/6"></div>
+
+          <div className='circle' id="5/0"></div>
+          <div className='circle' id="5/1"></div>
+          <div className='circle' id="5/2"></div>
+          <div className='circle' id="5/3"></div>
+          <div className='circle' id="5/4"></div>
+          <div className='circle' id="5/5"></div>
+          <div className='circle' id="5/6"></div>
+        </div>
     </main>
   );
 }
--- src/select/select.jsx
+++ src/select/select.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
+import "./select.css";
 
 export function Select({bgColor, textColor}) {
+    const [scores, setScores] = React.useState([]);
+
+    React.useEffect(() => {
+        // This will be replaced with a 3rd party service call
+        const scoresText = localStorage.getItem("scoreBoard")
+        if (scoresText) {
+            setScores(JSON.parse(scoresText));
+        }
+    }, []);
+
+    const rows = [];
+    if (scores.length) {
+        for (const [i, score] of scores.entries()) {
+            rows.push(
+                <tr key={i}>
+                    <td>{score.name}</td>
+                    <td>{score.wins}</td>
+                    <td>{score.loss}</td>
+                    <td>{score.wins/score.loss}</td>
+                </tr>
+            );
+        }
+    }
+    else {
+        rows.push(
+            <tr key="0">
+                <td colSpan="4">Gotta play to see your score</td>
+            </tr>
+        );
+    }
+
+    //temp code below until I can get the main game working
+    function settheScores() {
+        localStorage.setItem('scoreBoard', JSON.stringify([{"name":"bob","wins":5,"loss":4}]));
+  }
+
   return (
     <main style={{background: bgColor, color:textColor}}>
         <section className="player_s">
@@ -12,25 +49,23 @@ export function Select({bgColor, textColor}) {
             </div>
         </section>
 
+        <div className='div_index'>
+            <div>press button below to test scoreboard as the main game isn't finished</div>
+            <button className="btn btn-primary btn-dark" onClick={settheScores}>Set Score</button>
+        </div>
+
         <section>
             <h2>Leaderboards</h2>
             <table>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Wins</th>
-                        <th>Losses</th>
-                        <th>%</th>
+                        <td>Name</td>
+                        <td>Wins</td>
+                        <td>Losses</td>
+                        <td>%</td>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <th>enter</th>
-                        <th>enter</th>
-                        <th>enter</th>
-                        <th>enter</th>
-                    </tr>
-                </tbody>
+                <tbody>{rows}</tbody>
             </table>
         </section>
     </main>
